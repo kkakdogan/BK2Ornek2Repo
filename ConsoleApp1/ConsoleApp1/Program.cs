e9802f5e0bceaafd14e8e7686e62aeb125be3b9d
Kartal Kaan Akdoğan <kartalkaanakdogan@hotmail.com>
2022-03-17 14:58:35 +0300

    Console uygulaması ilk yükleme

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            for (int i = 0; i < 16; i++)
+            {
+                Console.WriteLine("i değeri: " + i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
